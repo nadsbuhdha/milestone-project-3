40ca63d48a1c16d99dcc9b20b9b38dcd13340340
nadsbuhdha <nadsbuhdha@gmail.com>
2021-12-30 17:52:54 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+def hangman_title():
+    print("""
+  _   _                                         
+ | | | | __ _ _ __   __ _ _ __ ___   __ _ _ __  
+ | |_| |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ |  _  | (_| | | | | (_| | | | | | | (_| | | | |
+ |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    |___/                       
+""")
+
+
+
+def main():
+    hangman_title()
+
+main()
